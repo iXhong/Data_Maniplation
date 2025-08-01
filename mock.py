f410e5b0d293ed7df2727d84cc047b01f3a4e5e3
George Liu <125779780+iXhong@users.noreply.github.com>
2025-08-01 16:33:13 +0800

    feat:generate mock data & plot errorbar for jk,bs

--- mock.py
+++ mock.py
@@ -2,27 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def data_gen():
+def data_gen(N):
     # 参数
-    T = 32  # 时间长度
+    T = 40
     m = 0.5  # 模拟质量
     A = 1.0  # 振幅
-    N_cfg = 10  # 配置数
-    noise_level = 0.02  # 噪声比例
+    N_cfg = N  # 配置数
+    noise_level = 0.8  # 噪声比例
 
-    # 时间点
-    t_vals = np.arange(T)
+    t_vals = np.linspace(0,5,T)
 
-    # 真实函数形状（无噪声）
-    true_C = A * (np.exp(-m * t_vals) - np.exp(-m * (T - t_vals)))
+    true_C = A * (np.exp(-m * t_vals))
 
     # 生成 N_cfg 个配置，每个加上随机噪声
     C_data = np.zeros((N_cfg, T))
     for i in range(N_cfg):
-        noise = np.random.normal(loc=0.0, scale=noise_level, size=T)
-        C_data[i] = true_C * (1 + noise)
+        noise = np.random.default_rng(i).normal(loc=0.0, scale=noise_level, size=T)
+        C_data[i] = true_C + noise
 
-    return C_data,t_vals
+    return true_C,C_data,t_vals
 
 
 def blocking(data,block_size):
@@ -39,6 +37,7 @@ def blocking(data,block_size):
 
 
 def jackknife(data):
+
     N,T = data.shape
     jk_samples = np.zeros((N,T))
 
@@ -51,23 +50,65 @@ def jackknife(data):
     return jk_mean,jk_err
 
 
-def eff_mass(data):
-    m_eff = np.log(data[:,:-1] / data[:,1:])
-    return m_eff
+def bootstrap(data,times):
+    
+    N,T = data.shape
+    bs_samples = np.zeros((times,T))
+    indices = np.arange(N)
+
+    for i in range(times):
+        chosen = np.random.choice(indices, N, replace=True)
+        bs_samples[i] = np.mean(data[chosen, :],axis=0)
+
+    mean = np.mean(bs_samples, axis=0)
+    variance = np.sum((bs_samples - mean) ** 2, axis=0) / (times - 1)
+    error = np.sqrt(variance)
+
+    return mean, error
 
 
-# data = np.loadtxt("./s3/TwoPt_ss_z2_conf210_mom000.dat",comments='#')
-# t = data[0:95, 4].astype(int)
-# C_real = data[0:95, 5]
-C_data,t = data_gen()
-m_data = eff_mass(C_data)
+def simple_method(data):
+    N = len(data)
+    mean = np.mean(data,axis=0)
+    std_dev = np.std(data,axis=0)
+    error = std_dev/np.sqrt(N)
+    return mean,error
 
-# data = blocking(m_data,2)
-# mean,error = jackknife(data)
 
-# print(f"mean={mean},error={error}")
+"""
+Test part
+"""
 
-# m_eff = eff_mass(C)
+if __name__ == "__main__":
+    N=1000
+    times = 2000
+    true_data,noise_data,t = data_gen(N)
+
+
+    mean = np.mean(noise_data,axis=0)
+    error = np.std(noise_data,axis=0)
+
+    jk_mean,jk_error = jackknife(noise_data)
+    bs_mean,bs_error = bootstrap(noise_data,times)
+
+
+    plt.figure(figsize=(10,7))
+    # plt.scatter(t_vals,C_data[0,:],label="C with noise")
+    plt.errorbar(t,mean,error,fmt='o',label="simple mean & error")
+    # plt.errorbar(t,jk_mean,jk_error,fmt='o',label="jackknife measurment")
+    # plt.errorbar(t,bs_mean,bs_error,fmt='.',label="bootstrap measurement")
+    plt.title(f"simple method,N_cfg={N}")
+    plt.xlabel('t')
+    plt.ylabel("y")
+    plt.plot(t,true_data,label="theory: y=e^(-0.5*t)")
+    # plt.savefig(f"bootstrap_N={N}_times={times}.png",dpi=300)
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+
+
 
 
 
